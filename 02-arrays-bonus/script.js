90b45a151c71ef3d59ce3d0d6b2bf873a219e040
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-12 16:03:32 +0100

    Created a single string with the elements of the array

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -47,5 +47,14 @@ console.log(isFabioPresent)
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+let teachersString = ""
+for (let i = 0; i < teachers.length; i++) {
+  const element = teachers[i];
 
-const teachersString = null;
+  if (i < teachers.length - 1) {
+    teachersString += `${element}, `
+  } else {
+    teachersString += element
+  }
+}
+console.log(teachersString)
